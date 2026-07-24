849c721218e2cee4a8eb19046c698fd6f7f928a8
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-07-24 11:08:01 +0200

    Move direct panel hit testing to test adapter

---
+++ tests/CSharpFar.Tests/ApplicationPanelInputHandlerTestExtensions.cs
@@ -0,0 +1,41 @@
+using CSharpFar.App.Input;
+using CSharpFar.App.Rendering;
+using CSharpFar.Console.Input;
+using CSharpFar.Core.Models;
+using CSharpFar.Ui;
+
+namespace CSharpFar.Tests;
+
+internal static class ApplicationPanelInputHandlerTestExtensions
+{
+    public static ApplicationInputHandlingResult Handle(
+        this ApplicationPanelInputHandler handler,
+        MouseConsoleInputEvent input,
+        ApplicationPanelFrame? frame,
+        UiInputRouteKind routeKind)
+    {
+        if (frame is null)
+            return ApplicationInputHandlingResult.NotHandled;
+
+        UiTargetId? target = ApplicationTargetIds.Panel(frame.Side);
+        if (routeKind == UiInputRouteKind.HitTarget)
+        {
+            var builder = new UiInteractionFrameBuilder()
+                .AddHitRegion(ApplicationTargetIds.Panel(frame.Side), frame.Bounds);
+            if (frame.RetryBounds is { } retryBounds)
+                builder.AddHitRegion(ApplicationTargetIds.PanelRetry(frame.Side), retryBounds);
+            foreach (ApplicationPanelItemHit item in frame.VisibleItems)
+            {
+                UiTargetId itemTarget = ApplicationTargetIds.PanelItem(frame.Side, item.ItemIndex);
+                builder.AddHitRegion(itemTarget, item.Bounds);
+                if (frame.Side == PanelSide.Right && item.Bounds.X == frame.Bounds.X + 1)
+                    builder.AddHitRegion(itemTarget, new Rect(frame.Bounds.X, item.Bounds.Y, 1, item.Bounds.Height));
+            }
+
+            if (builder.Build().TryHitTest(input.X, input.Y, out UiHitRegion hit))
+                target = hit.Target;
+        }
+
+        return handler.Handle(input, frame, routeKind, target);
+    }
+}
